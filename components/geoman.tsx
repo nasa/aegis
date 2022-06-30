90163a10362f561fa0421965244c9fa4454f4041
Ben Feist <bf@benfeist.com>
2022-06-30 19:57:26 -0400

    Some progress on understanding Leaflet tile layers

--- components/geoman.tsx
+++ components/geoman.tsx
@@ -1,34 +1,89 @@
 import L from "leaflet";
 import "@geoman-io/leaflet-geoman-free";
 import "@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css";
+import styles from "./geoman.module.css";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 
 L.Icon.Default.imagePath = "/leaflet/images/";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
-const center = [51.505, -0.09] as L.LatLngExpression;
+// const center = [51.505, -0.09] as L.LatLngExpression; // London
+const center = [64.833445, -16.378351] as L.LatLngExpression; // Iceland
 const zoom = 13;
+
+const layerBaseURL = `http://192.168.0.5:8005/NASA_AEGIS/Missions/Iceland_v001/Layers/`;
 
 const Geoman = () => {
   const mapRef = useRef(null);
+
+  const [layerList, setLayerList] = useState([]);
+  const [opacities, setOpacities] = useState({});
+
   useEffect(() => {
+    if (!mapRef.current) {
+      return;
+    }
     mapRef.current = L.map("map", {
       center: center,
       zoom: zoom,
-      layers: [
-        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-          attribution: "",
-        }),
-      ],
     });
+
+    const newPane = mapRef.current.createPane("newPane");
+    newPane.style.zIndex = "1";
+
+    mapRef.current.addLayer(
+      L.tileLayer(layerBaseURL + "Landsat_TrueColor/{z}/{x}/{y}.png", {
+        tileSize: 256,
+        bounds: [
+          [64.30447800311914, -17.87031300021185],
+          [65.31466820647802, -14.9580710002728],
+        ],
+        tms: true,
+        minZoom: 6,
+        maxZoom: 11,
+        id: "L8_RGB_30m",
+        pane: "newPane",
+      })
+    );
+
+    // Iceland large regional hillslope raster layer
+    mapRef.current.addLayer(
+      L.tileLayer(layerBaseURL + "LargeRegional_5m_Hillslope/{z}/{x}/{y}.png", {
+        tileSize: 256,
+        bounds: [
+          [64.30454312899785, -17.87031300021185],
+          [65.3153669180373, -14.9580710002728],
+        ],
+        tms: true,
+        minZoom: 6,
+        maxZoom: 15,
+        opacity: 0.5,
+        pane: "newPane",
+        id: "hillshade_5m",
+      })
+    );
 
     mapRef.current.pm.addControls({
       drawMarker: true,
     });
 
+    mapRef.current.on("pm:globaldrawmodetoggled", (e) => {
+      console.log("Global Draw Toggled", e);
+    });
+
+    // listen to vertexes being added to currently drawn layer (called workingLayer)
+    mapRef.current.on("pm:drawstart", ({ workingLayer }) => {
+      console.log("Draw start");
+      workingLayer.on("pm:vertexadded", (e) => {
+        console.log("Vertex added", e);
+      });
+    });
+
     mapRef.current.on("pm:create", (e) => {
       if (e.layer && e.layer.pm) {
         const shape = e;
-        console.log(e);
+        console.log("Create", e);
 
         // enable editing of circle
         shape.layer.pm.enable();
@@ -47,10 +102,17 @@ const Geoman = () => {
 
     mapRef.current.on("pm:remove", () => {
       console.log("object removed");
-      // console.log(mapRef.current.pm.getGeomanLayers(true).toGeoJSON());
+      console.log(mapRef.current.pm.getGeomanLayers(true).toGeoJSON());
     });
 
+    let layers = [];
+    mapRef.current.eachLayer(function (layer) {
+      layers.push(layer);
+    });
+    setLayerList(layers);
+
     return () => {
+      console.log("UseEffect return");
       mapRef.current.pm.removeControls();
       mapRef.current.pm.setGlobalOptions({ pmIgnore: true });
       mapRef.current.off();
@@ -60,9 +122,45 @@ const Geoman = () => {
 
   return (
     <>
-      <div id="map" style={{ width: "100%", height: "100vh" }}></div>
+      <div id="map" className={styles.map} ref={mapRef}></div>
+      <div className={styles.topright}>
+        <div className={styles.menu}>
+          <div className={styles.title}>Map Layers (Iceland)</div>
+          <div>{makeLayerControls()}</div>
+
+          <div className={styles.options}>
+            <div className={styles.title} style={{ padding: "0" }}>
+              Options
+            </div>
+            <button id="houstonbutton">Center on Houston</button>
+            <button id="icelandbutton">Center on Iceland</button>
+          </div>
+        </div>
+      </div>
     </>
   );
+
+  function makeLayerControls() {
+    if (!mapRef.current || layerList.length === 0) {
+      return;
+    }
+    const layers = layerList.map((layer) => {
+      return (
+        <div className={styles.layer_container}>
+          <Slider
+            className={styles.slider}
+            min={0}
+            max={100}
+            defaultValue={100}
+            onChange={(value) => {
+              layer.setOpacity((value as number) / 100);
+            }}
+          />
+        </div>
+      );
+    });
+    return layers;
+  }
 };
 
 export default Geoman;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import "../styles/globals.css";
 import "leaflet/dist/leaflet.css";
 import "leaflet-draw/dist/leaflet.draw.css";
+import "../styles/fonts.css";
+
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
