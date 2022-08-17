4b7d35c16a5a76f07e44d6f56f3d4eb8eb5157e1
James Montalvo <edwin.j.montalvo@nasa.gov>
2022-08-17 08:36:12 -0500

    fix: only use secure cookie in prod, allow dev http

--- server/session/config.ts
+++ server/session/config.ts
@@ -3,4 +3,7 @@
 export const ironOptions = {
   cookieName: "aegis-session",
   password: "6cXV-%SMN9Pfpr6?m<ALs[XXqh=!jq_u",
+  cookieOptions: {
+    secure: process.env.NODE_ENV === "production",
+  },
 };
