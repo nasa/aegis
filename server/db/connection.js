150b7ae0f779b9aa391e715be4ff4177f63d4bb5
James Montalvo <edwin.j.montalvo@nasa.gov>
2022-09-08 07:41:27 -0500

    refactor: only have one .env file, have docker use it too

--- server/db/connection.js
+++ server/db/connection.js
@@ -3,11 +3,11 @@ import Sequelize from "sequelize";
 export function getMMGISSequelizeConnection() {
   // create a sequelize instance with our local postgres database information.
   const sequelize = new Sequelize(
-    process.env.MMGIS_DB_NAME,
-    process.env.MMGIS_DB_USER,
-    process.env.MMGIS_DB_PASS,
+    process.env.AEGIS_DB_NAME,
+    process.env.AEGIS_DB_USER,
+    process.env.AEGIS_DB_PASS,
     {
-      host: process.env.MMGIS_DB_HOST,
+      host: process.env.AEGIS_DB_HOST,
       dialect: "postgres",
       logging: process.env.VERBOSE_LOGGING == "true" || false,
       pool: {
@@ -40,44 +40,3 @@ export function getMMGISSequelizeConnection() {
 
   return sequelize;
 }
-
-// export function getAEGISSequelizeConnection() {
-//   // create a sequelize instance with our local postgres database information.
-//   const sequelize = new Sequelize(
-//     process.env.AEGIS_DB_NAME,
-//     process.env.AEGIS_DB_USER,
-//     process.env.AEGIS_DB_PASS,
-//     {
-//       host: process.env.AEGIS_DB_HOST,
-//       dialect: "postgres",
-//       logging: process.env.VERBOSE_LOGGING == "true" || false,
-//       pool: {
-//         max: 10,
-//         min: 0,
-//         acquire: 30000,
-//         idle: 10000,
-//       },
-//     }
-//   );
-
-//   sequelize
-//     .authenticate()
-//     .then(() => {
-//       console.log(
-//         "info",
-//         "AEGIS Database connection has successfully been established.",
-//         "connection"
-//       );
-//     })
-//     .catch((err) => {
-//       console.log(
-//         "infrastructure_error",
-//         "Unable to connect to the database.",
-//         "connection",
-//         null,
-//         err
-//       );
-//     });
-
-//   return sequelize;
-// }
