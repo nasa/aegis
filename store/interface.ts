46e24a47605a033995dc487020b1a64f15ebf6fa
Ben Feist <bf@benfeist.com>
2022-12-14 11:12:33 -0500

    layers default view

--- store/interface.ts
+++ store/interface.ts
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const initialState: InterfaceState = {
-  sectionSelectedLabel: "poi",
+  sectionSelectedLabel: "map_layer_selector",
 };
 
 export const interfaceSlice = createSlice({
