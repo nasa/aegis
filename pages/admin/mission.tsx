fcb78e0563a3c85002e319b70889e742c4903e19
David Rynearson <david.c.rynearson@nasa.gov>
2023-03-23 16:20:14 -0500

    114a import json config

--- pages/admin/mission.tsx
+++ pages/admin/mission.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from "next";
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { ChangeEvent, Dispatch, SetStateAction, useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { isAdmin, isLoggedIn } from "http-client/internal-api";
 import { getMissions, deleteMission, upsertMission } from "http-client/mission";
@@ -17,6 +17,7 @@ import adminStyles from "components/admin/admin.module.css";
 import { deleteFile } from "http-client/file";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowAltCircleLeft } from "@fortawesome/free-regular-svg-icons";
+import { faCopy, faPaste } from "@fortawesome/free-solid-svg-icons";
 
 const Mission: NextPage = () => {
   const router = useRouter();
@@ -24,6 +25,7 @@ const Mission: NextPage = () => {
   const [editMissionId, setEditMissionId] = useState<number>(); //track mission currently in edit
   const [mission, setMission] = useState<Mission>(); //current mission being edited
   const [admin, setAdmin] = useState<boolean>(false);
+
   async function loadMissionsFromDB() {
     const missionList = (await getMissions()).data;
     setMissions(missionList);
@@ -45,7 +47,7 @@ const Mission: NextPage = () => {
       const response = await isLoggedIn(); //check user is logged in
       const adminResponse = await isAdmin(); //check user is admin
       if (response.status !== "success" || !adminResponse.data["admin"]) {
-        router.push("/"); //user is not logged in or an admin. Redirect to homepage
+        await router.push("/"); //user is not logged in or an admin. Redirect to homepage
       } else {
         setAdmin(true);
       }
@@ -188,6 +190,7 @@ const AddEditMission = (props: {
 }) => {
   const { refreshMissionList, mission, setMission } = { ...props };
   const [config, setConfig] = useState<Config>(createNewConfig());
+  const [errorTriggered, setErrorTriggered] = useState<boolean>(false);
 
   useEffect(() => {
     if (props.mission) {
@@ -205,113 +208,183 @@ const AddEditMission = (props: {
     alert(`${res.status} - ${res.message}`);
   }
 
+  async function onChangeHandler(
+    event: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>
+  ) {
+    const { name, value } = event.target;
+    switch (name) {
+      case "missionConfig":
+        if (isValidJsonString(value)) {
+          setConfig(JSON.parse(value));
+        } else {
+          if (value === "") {
+            setConfig(JSON.parse(value));
+            return;
+          }
+          setConfig(undefined);
+        }
+        break;
+    }
+  }
+
+  function isValidJsonString(str: string) {
+    try {
+      JSON.parse(str);
+    } catch (e) {
+      return false;
+    }
+    return true;
+  }
+
   return (
     mission && (
-      <div className={adminStyles.editMissionDiv}>
-        {mission.id ? <h3>Edit Mission &quot;{mission.name}&quot;</h3> : <h3>Add Mission</h3>}
-        <button
-          type="button"
-          onClick={() => {
-            saveMission();
-          }}
-        >
-          Save Mission
-        </button>
-        <br />
-        <br />
-        <div className={adminStyles.sectionDiv}>
-          Manage files in the /Data folder for this mission
+      <div className={styles.container}>
+        <div className={adminStyles.editMissionDiv}>
+          {mission.id ? <h3>Edit Mission &quot;{mission.name}&quot;</h3> : <h3>Add Mission</h3>}
+          <button
+            type="button"
+            onClick={() => {
+              saveMission();
+            }}
+          >
+            Save Mission
+          </button>
           <br />
           <br />
-          {mission.id ? (
-            <>
-              <FileManager path={`missionFiles/${mission.id}/Data`} />
-            </>
-          ) : (
-            <div>A new mission must be saved first before you can upload files</div>
-          )}
-        </div>
-        <br />
-        <br />
-        <div id="missionDiv">
-          <div className={styles.editDiv}>
-            <label htmlFor="newName">Mission Name (Parent)</label>
+          <div className={adminStyles.sectionDiv}>
+            Manage files in the /Data folder for this mission
+            <br />
+            <br />
+            {mission.id ? (
+              <>
+                <FileManager path={`missionFiles/${mission.id}/Data`} />
+              </>
+            ) : (
+              <div>A new mission must be saved first before you can upload files</div>
+            )}
           </div>
-          <div className={styles.editDiv}>
-            <input
-              id="newName"
-              type="text"
-              onChange={(e) => {
-                setMission({ ...mission, name: e.target.value });
-              }}
-              value={mission?.name}
+          <br />
+          <br />
+          <div id="missionDiv">
+            <div className={styles.editDiv}>
+              <label htmlFor="newName">Mission Name (Parent)</label>
+            </div>
+            <div className={styles.editDiv}>
+              <input
+                id="newName"
+                type="text"
+                onChange={(e) => {
+                  setMission({ ...mission, name: e.target.value });
+                }}
+                value={mission?.name}
+              />
+            </div>
+          </div>
+          <div id="landerLatDiv">
+            <div className={styles.editDiv}>
+              <label htmlFor="landerLat">Lander Location Latitude</label>
+            </div>
+            <div className={styles.editDiv}>
+              <input
+                id="landerLat"
+                type="text"
+                onChange={(e) => {
+                  setMission({
+                    ...mission,
+                    landerLocation: { ...mission.landerLocation, lat: +e.target.value },
+                  });
+                }}
+                value={mission?.landerLocation?.lat || ""}
+              />
+            </div>
+          </div>
+          <div id="landerLongDiv">
+            <div className={styles.editDiv}>
+              <label htmlFor="landerLong">Lander Location Longitude</label>
+            </div>
+            <div className={styles.editDiv}>
+              <input
+                id="landerLong"
+                type="text"
+                onChange={(e) => {
+                  setMission({
+                    ...mission,
+                    landerLocation: { ...mission.landerLocation, lng: +e.target.value },
+                  });
+                }}
+                value={mission?.landerLocation?.lng || ""}
+              />
+            </div>
+          </div>
+          <div id="traverseDiv">
+            <div className={styles.editDiv}>
+              <label htmlFor="traverse">Default Traverse Speed</label>
+            </div>
+            <div className={styles.editDiv}>
+              <input
+                id="traverse"
+                type="text"
+                onChange={(e) => {
+                  setMission({
+                    ...mission,
+                    traverseSpeed: +e.target.value,
+                  });
+                }}
+                value={mission?.traverseSpeed}
+              />
+            </div>
+          </div>
+          <MSV config_msv={config?.msv} setConfig={setConfig} />
+          <Tools config_tools={config?.tools} setConfig={setConfig} />
+          <Projection config_projection={config?.projection} setConfig={setConfig} />
+          <Look config_look={config?.look} setConfig={setConfig} />
+          <Panels
+            config_panels={config?.panels}
+            config_panelSettings={config?.panelSettings}
+            setConfig={setConfig}
+          />
+          <Time config_time={config?.time} setConfig={setConfig} />
+        </div>
+        <div className={styles.rightFlexCenter}>
+          <div className={styles.configDiv}>
+            <label className={styles.title} htmlFor="configImport">
+              <span className={styles.label}>Mission Config (json)</span>
+              <FontAwesomeIcon
+                title="Paste from Clipboard"
+                icon={faPaste}
+                className={styles.actionIcon}
+                onClick={() => {
+                  navigator.clipboard.readText().then((text) => {
+                    if (isValidJsonString(text)) {
+                      setConfig(JSON.parse(text));
+                      setErrorTriggered(false);
+                    } else {
+                      setErrorTriggered(true);
+                    }
+                  });
+                }}
+              />
+              <FontAwesomeIcon
+                title="Copy Json"
+                icon={faCopy}
+                className={styles.actionIcon}
+                onClick={() => {
+                  navigator.clipboard.writeText(JSON.stringify(config, null, 2));
+                }}
+              />
+            </label>
+
+            <textarea
+              id="configImport"
+              className={styles.configImport}
+              value={JSON.stringify(config, null, 2)}
+              onChange={onChangeHandler}
             />
+            <span className={styles.errorMessage}>
+              {errorTriggered === true ? "Invalid JSON" : ""}
+            </span>
           </div>
         </div>
-        <div id="landerLatDiv">
-          <div className={styles.editDiv}>
-            <label htmlFor="landerLat">Lander Location Latitude</label>
-          </div>
-          <div className={styles.editDiv}>
-            <input
-              id="landerLat"
-              type="text"
-              onChange={(e) => {
-                setMission({
-                  ...mission,
-                  landerLocation: { ...mission.landerLocation, lat: +e.target.value },
-                });
-              }}
-              value={mission?.landerLocation?.lat || ""}
-            />
-          </div>
-        </div>
-        <div id="landerLongDiv">
-          <div className={styles.editDiv}>
-            <label htmlFor="landerLong">Lander Location Longitude</label>
-          </div>
-          <div className={styles.editDiv}>
-            <input
-              id="landerLong"
-              type="text"
-              onChange={(e) => {
-                setMission({
-                  ...mission,
-                  landerLocation: { ...mission.landerLocation, lng: +e.target.value },
-                });
-              }}
-              value={mission?.landerLocation?.lng || ""}
-            />
-          </div>
-        </div>
-        <div id="traverseDiv">
-          <div className={styles.editDiv}>
-            <label htmlFor="traverse">Default Traverse Speed</label>
-          </div>
-          <div className={styles.editDiv}>
-            <input
-              id="traverse"
-              type="text"
-              onChange={(e) => {
-                setMission({
-                  ...mission,
-                  traverseSpeed: +e.target.value,
-                });
-              }}
-              value={mission?.traverseSpeed}
-            />
-          </div>
-        </div>
-        <MSV config_msv={config?.msv} setConfig={setConfig} />
-        <Tools config_tools={config?.tools} setConfig={setConfig} />
-        <Projection config_projection={config?.projection} setConfig={setConfig} />
-        <Look config_look={config?.look} setConfig={setConfig} />
-        <Panels
-          config_panels={config?.panels}
-          config_panelSettings={config?.panelSettings}
-          setConfig={setConfig}
-        />
-        <Time config_time={config?.time} setConfig={setConfig} />
       </div>
     )
   );
