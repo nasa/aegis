61ec0d3db0100531eb21ca1eb0de220cf6532ec2
Luke McSherry <luke.a.mcsherry@nasa.gov>
2023-06-22 09:59:34 -0500

    changed custom-dictionaries name to custom-names to avoid confusion

--- utils/unique-name.ts
+++ utils/unique-name.ts
@@ -7,7 +7,7 @@ import {
   names,
   starWars,
 } from "unique-names-generator";
-import starTrek from "../public/custom-dictionaries/star-trek-dict";
+import starTrek from "../public/custom-names/star-trek-names";
 const profanityFilter = require("leo-profanity");
 
 type DictName =
