214bbeec16cd1e1c7a207daae40a111645350bb2
Luke McSherry <luke.a.mcsherry@nasa.gov>
2023-07-03 10:32:18 -0500

    Added admin recovery capabilities

--- pages/api/users/adminRecovery.ts
+++ pages/api/users/adminRecovery.ts
@@ -3,8 +3,12 @@ import { withIronSessionApiRoute } from "iron-session/next";
 
 import { ironOptions } from "server/session/config";
 import { IronSessionData } from "iron-session";
+import _ from "lodash";
 import bcrypt from "bcryptjs";
-import { upsertUser } from "../../../http-client/user";
+import { User as User_db } from "server/database/models/user.model";
+import { getEM } from "utils/mikro";
+import { EntityData } from "@mikro-orm/core";
+import { roundDateToSecond } from "utils/formatting";
 
 export default withIronSessionApiRoute(handler, ironOptions);
 
@@ -13,18 +17,14 @@ async function handler(
   res: NextApiResponse<WrappedResponse<IronSessionData>>
 ) {
   try {
-    if (req.method == "POST") {
-      const recoveryKey = req.body.recoveryKey;
+    if (req.method == "GET") {
+      const recoveryKey = req.query.recoveryKey as string;
       if (recoveryKey === process.env.ADMIN_RECOVERY_KEY) {
         //Add default values back into admin user
-        upsertUser({
-          id: 1,
-          username: "admin",
-          password: await bcrypt.hash("admin", 10),
-          email: "admin@localhost",
-          permissionList: [],
-          adminPermission: true,
-        });
+        await upsertAdmin();
+        res.status(200).json({ status: "success", message: "Admin user updated" });
+      } else {
+        res.status(500).json({ status: "error", message: "Recovery Key does not match" });
       }
     } else {
       throw new Error("Method not allowed");
@@ -32,4 +32,55 @@ async function handler(
   } catch (error) {
     res.status(500).json({ status: "error", message: "Unexpected error :" + error });
   }
+}
+
+/**
+ * Inserts or Updates a user into the database
+ * @returns a copy of the user object that was upserted
+ * @param user
+ */
+async function upsertAdmin(): Promise<User> {
+  const adminCreds: User = {
+    id: 1,
+    username: "admin",
+    password: await bcrypt.hash("admin", 10),
+    email: "admin@localhost",
+    permissionList: [],
+    adminPermission: true,
+  };
+
+  const em = getEM();
+  const userCopy: User = _.cloneDeep(adminCreds);
+  const salt = await bcrypt.genSalt();
+
+  const upsertRecord: EntityData<User_db> = {
+    ...userCopy,
+    updatedAt: new Date(userCopy.updatedAt),
+    createdAt: new Date(userCopy.createdAt),
+  };
+
+  if (String(upsertRecord.createdAt) === "Invalid Date") {
+    upsertRecord.createdAt = new Date("1969-07-20");
+  }
+
+  const updateDate = roundDateToSecond(new Date());
+  upsertRecord.updatedAt = updateDate;
+
+  let existingUser = await em.findOne(User_db, { id: userCopy.id });
+
+  if (!existingUser) {
+    return null;
+  }
+  upsertRecord.permissionList = existingUser.permissionList;
+  if (userCopy.password !== existingUser.password) {
+    upsertRecord.password = await bcrypt.hash("admin", salt);
+  }
+
+  existingUser = em.assign(existingUser, upsertRecord);
+  await em.persistAndFlush(existingUser);
+  return {
+    ...userCopy,
+    updatedAt: existingUser.updatedAt.toISOString(),
+    createdAt: existingUser.createdAt.toISOString(),
+  } as User;
 }
