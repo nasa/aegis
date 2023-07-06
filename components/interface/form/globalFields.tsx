a6b6c28757ac7f1a8e69d0248e39e0fda2d29ebc
Luke McSherry <luke.a.mcsherry@nasa.gov>
2023-07-06 08:36:04 -0500

    missed removing a console.log

--- components/interface/form/globalFields.tsx
+++ components/interface/form/globalFields.tsx
@@ -80,7 +80,6 @@ export const TextboxButton: FunctionComponent<{
       data-tooltip-html={toolTip}
       onMouseDown={(e) => {
         onMouseDown(e);
-        console.log(active);
       }}
       style={style}
     >
