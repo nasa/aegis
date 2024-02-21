fa8c965761a0b52276dfce49c43ed94bce018f33
Luke McSherry <luke.a.mcsherry@nasa.gov>
2024-02-21 05:17:04 -0600

    Added mini-menu to action templates

---
+++ src/components/panes/mission-actionTemplates-menu.tsx
@@ -0,0 +1,74 @@
+import { faClone, faEllipsisV, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FunctionComponent, useRef } from "react";
+import { useAppDispatch } from "utils/useAppDispatch";
+import actionStyles from "./actions-action.module.css";
+import { thunkDuplicateActionTemplate, thunkDeleteActionTemplate } from "store/thunk/thunkMission";
+
+export const ActionTemplateMenu: FunctionComponent<{
+  actionTemplate: ActionTemplate;
+}> = ({ actionTemplate }) => {
+  const dispatch = useAppDispatch();
+  const dialogRef = useRef(null);
+  const menuRef = useRef(null);
+
+  const handleMenuOpen = (e: React.MouseEvent) => {
+    const x = e.clientX - 130; // width of the menu
+    menuRef.current.style.left = `${x}px`;
+    menuRef.current.style.top = `${e.clientY}px`;
+  };
+
+  return (
+    <>
+      <dialog
+        ref={dialogRef}
+        className={actionStyles.menuContainer}
+        onClick={() => {
+          dialogRef.current?.close();
+        }}
+      >
+        <div ref={menuRef} className={actionStyles.templateMenu}>
+          <div
+            className={actionStyles.menuItem}
+            onClick={() => {
+              dispatch(thunkDuplicateActionTemplate({ actionTemplateUuid: actionTemplate.uuid }));
+              dialogRef.current?.close();
+            }}
+          >
+            <div className={actionStyles.menuItemIcon}>
+              <FontAwesomeIcon icon={faClone} size="lg" />
+            </div>
+            <div className={actionStyles.menuItemText}>Duplicate Template</div>
+          </div>
+          <div
+            className={actionStyles.menuItem}
+            onClick={(e) => {
+              if (window.confirm("Are you sure you want to delete this Action Template?")) {
+                dispatch(thunkDeleteActionTemplate({ actionTemplateUuid: actionTemplate.uuid }));
+                e.stopPropagation();
+              }
+              dialogRef.current?.close();
+            }}
+          >
+            <div className={actionStyles.menuItemIcon}>
+              <FontAwesomeIcon icon={faTrashAlt} size="sm" />
+            </div>
+            <div className={actionStyles.menuItemText}>Delete Template</div>
+          </div>
+        </div>
+      </dialog>
+
+      <FontAwesomeIcon
+        icon={faEllipsisV}
+        size="sm"
+        onClick={(e) => {
+          handleMenuOpen(e);
+          dialogRef.current?.showModal();
+          e.stopPropagation();
+        }}
+        style={{ marginTop: "3px", width: "15px", color: "var(--grey5)", outline: "none" }}
+        tabIndex={0}
+      />
+    </>
+  );
+};
