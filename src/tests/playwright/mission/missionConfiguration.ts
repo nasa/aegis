a94337294871967469d8c5b1c6bf29f4f4db12b0
lmcsherr <luke.a.mcsherry@nasa.gov>
2024-07-24 13:01:58 -0500

    small linting fix

--- src/tests/playwright/mission/missionConfiguration.ts
+++ src/tests/playwright/mission/missionConfiguration.ts
@@ -1,14 +1,14 @@
-import { Page, expect } from "@playwright/test";
+// import { Page, expect } from "@playwright/test";
 
-export async function landerRadiiTest(page: Page): Promise<string> {
-  await page.goto("http://aegis-local.fit.nasa.gov:4000/mission/1");
-  //go to mission preferences
-  await page.waitForTimeout(2000);
-  await page.getByLabel("mission Section", { exact: true }).click();
-  await expect(page.getByLabel("leftPanelTitle", { exact: true })).toContainText(
-    "Mission Configuration"
-  );
-  await expect(page.getByLabel("rightBodyTitle", { exact: true })).toContainText(
-    "Mission Preferences"
-  );
-}
+// export async function landerRadiiTest(page: Page): Promise<string> {
+//   await page.goto("http://aegis-local.fit.nasa.gov:4000/mission/1");
+//   //go to mission preferences
+//   await page.waitForTimeout(2000);
+//   await page.getByLabel("mission Section", { exact: true }).click();
+//   await expect(page.getByLabel("leftPanelTitle", { exact: true })).toContainText(
+//     "Mission Configuration"
+//   );
+//   await expect(page.getByLabel("rightBodyTitle", { exact: true })).toContainText(
+//     "Mission Preferences"
+//   );
+// }
