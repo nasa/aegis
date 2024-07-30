a7a9c8ed2c41f6a9b3030157c85f085d1a57818f
lmcsherr <luke.a.mcsherry@nasa.gov>
2024-07-30 14:56:42 -0500

    prettier changes on a migration file? Was failing a prettier test before so should work now

--- src/server/database/migrations/Migration20240730193530.ts
+++ src/server/database/migrations/Migration20240730193530.ts
@@ -1,10 +1,11 @@
-import { Migration } from '@mikro-orm/migrations';
+import { Migration } from "@mikro-orm/migrations";
 
 export class Migration20240730193530 extends Migration {
-
   async up(): Promise<void> {
     this.addSql('alter table "rex_db" add column "owner_id" int null;');
-    this.addSql('alter table "rex_db" add constraint "rex_db_owner_id_foreign" foreign key ("owner_id") references "user_db" ("id") on update cascade on delete set null;');
+    this.addSql(
+      'alter table "rex_db" add constraint "rex_db_owner_id_foreign" foreign key ("owner_id") references "user_db" ("id") on update cascade on delete set null;'
+    );
   }
 
   async down(): Promise<void> {
@@ -12,5 +13,4 @@ export class Migration20240730193530 extends Migration {
 
     this.addSql('alter table "rex_db" drop column "owner_id";');
   }
-
 }
