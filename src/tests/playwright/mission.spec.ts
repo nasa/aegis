b35bbb9b70cc0c106da6c9ca2034f3bc19053e47
Luke McSherry <luke.a.mcsherry@nasa.gov>
2025-05-29 14:41:24 -0500

    renamed files back to original name

--- src/tests/playwright/mission.spec.ts
+++ src/tests/playwright/mission.spec.ts
@@ -1,9 +1,9 @@
 import { test } from "@playwright/test";
-import { actionTemplatesTest } from "./mission/actionTemplateTest.spec";
-import { missionConfigTest } from "./mission/missionConfigurationTest.spec";
-import { circleDefinitionsTest } from "./mission/circleDefinitionsTest.spec";
-import { missionEquipmentTest } from "./mission/missionEquipmentTest.spec";
-import { presetTest } from "./mission/presetTest.spec";
+import { actionTemplatesTest } from "./mission/actionTemplateTest";
+import { missionConfigTest } from "./mission/missionConfigurationTest";
+import { circleDefinitionsTest } from "./mission/circleDefinitionsTest";
+import { missionEquipmentTest } from "./mission/missionEquipmentTest";
+import { presetTest } from "./mission/presetTest";
 
 test("edit mission preferences", async ({ page }) => {
   await missionConfigTest(page);
