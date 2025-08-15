d92e8c7e20242ee32dc425da46919c1529b3ffd2
Luke McSherry <luke.a.mcsherry@nasa.gov>
2025-08-15 15:14:45 -0500

    Surf nav changes

--- src/tests/jest/utils/surfNav/coordinates.test.ts
+++ src/tests/jest/utils/surfNav/coordinates.test.ts
@@ -1,3 +1,14 @@
+/*
+ * The following test cases should EXACTLY match the test cases from the surf_nav repo
+ * They should ONLY be changed to match tests in the surf_nav repo.
+ * You can access these tests in the surf_nav repo at:
+ * https://gitlab.fit.nasa.gov/cm-artemis-integration/cm-ehp/surf-nav
+ *
+ * and the file these tests are in is:
+ * https://gitlab.fit.nasa.gov/cm-artemis-integration/cm-ehp/surf-nav/-/blob/main/surf_nav/nav_tools/test/test_coordinates.py?ref_type=heads
+ *
+ * The following test cases are also EXPORT CONTROLLED! Authorization is required to export or reuse these items
+ */
 import {
   latlong_to_lps,
   lps_to_latlong,
--- src/tests/jest/utils/surfNav/orienteering.test.ts
+++ src/tests/jest/utils/surfNav/orienteering.test.ts
@@ -1,3 +1,15 @@
+/*
+ * The following test cases should EXACTLY match the test cases from the surf_nav repo
+ * They should ONLY be changed to match tests in the surf_nav repo.
+ * You can access these tests in the surf_nav repo at:
+ * https://gitlab.fit.nasa.gov/cm-artemis-integration/cm-ehp/surf-nav
+ *
+ * and the file these tests are in is:
+ * https://gitlab.fit.nasa.gov/cm-artemis-integration/cm-ehp/surf-nav/-/blob/main/surf_nav/nav_tools/test/test_orienteering.py?ref_type=heads
+ *
+ * The following test cases are also EXPORT CONTROLLED! Authorization is required to export or reuse these items
+ */
+
 import {
   xy_from_range_bearing,
   range_bearing_from_xy,
