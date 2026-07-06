ae9052054e65b6cd75c4b42b1890e6ce0f64638f
Ben Feist <benjamin.f.feist@nasa.gov>
2026-07-06 12:34:54 -0400

    Update rule list layout to not take up so much horizontal space

--- src/components/panes/stm-rules/stm-rules-tab-rules.tsx
+++ src/components/panes/stm-rules/stm-rules-tab-rules.tsx
@@ -1,9 +1,11 @@
-import type { FunctionComponent } from "react";
+import type { CSSProperties, FunctionComponent } from "react";
 import styles from "./stm-rules-page.module.css";
+import tableStyles from "./stm-rules-list-table.module.css";
 import STMRulesTable from "./stm-rules-list-table";
-import { refEqual } from "utils/useAppSelector";
+import { deepEqual, refEqual, shallowEqual, useAppSelector } from "utils/useAppSelector";
 import { useMissionDocSelector } from "utils/useDocSelector";
 import { StmTierTitle, useStmTierExpansion } from "./stm-rules-tier-titles";
+import { useStmRuleColumnWidths } from "./stm-rule-column-widths";
 
 /**
  * "Rules" tab: STM hierarchy with per-level3 satisfaction rules and inline
@@ -12,9 +14,24 @@ import { StmTierTitle, useStmTierExpansion } from "./stm-rules-tier-titles";
 const StmRulesTabRules: FunctionComponent = () => {
   const { stmLevel1Enabled, tierColumns } = useStmTierExpansion();
   const stmLevel3Name = useMissionDocSelector((mission) => mission.stmLevel3Name, refEqual);
+  const rules = useAppSelector((state) => state.stm.rules, shallowEqual);
+  const actionDefinitions = useMissionDocSelector(
+    (mission) => mission.actionDefinitions,
+    deepEqual
+  );
+  const { widths, fontRef } = useStmRuleColumnWidths(rules, actionDefinitions);
+
+  const widthVars = {
+    "--stmVerbColWidth": `${widths.verbWidth}px`,
+    "--stmNounColWidth": `${widths.nounWidth}px`,
+    "--stmAdjectiveColWidth": `${widths.adjectiveWidth}px`,
+    // count + two conjunctions + paddings, so the header labels line up roughly.
+    "--stmRulesRegionWidth": `calc(var(--stmVerbColWidth) + var(--stmNounColWidth) + var(--stmAdjectiveColWidth) + 90px)`,
+  } as CSSProperties;
 
   return (
-    <>
+    <div className={styles.rulesTabWrapper} style={widthVars}>
+      <div ref={fontRef} className={`${tableStyles.stmTables} ${styles.fontProbe}`} />
       <div
         className={styles.listHeaderTitles}
         style={{ gridTemplateColumns: [...tierColumns, "285px", "auto"].join(" ") }}
@@ -23,10 +40,13 @@ const StmRulesTabRules: FunctionComponent = () => {
         <StmTierTitle tier="level2" />
         <div className={styles.listTableTitle}>{stmLevel3Name}s</div>
         <div className={styles.listTableRuleTitleContainer}>
-          <div className={styles.listTableTitle} style={{ flex: "1 1 auto" }}>
+          <div
+            className={styles.listTableTitle}
+            style={{ flex: "0 0 auto", width: "var(--stmRulesRegionWidth)" }}
+          >
             Satisfaction Rules
           </div>
-          <div className={styles.listTableTitle} style={{ flex: "0 0 240px" }}>
+          <div className={styles.listTableTitle} style={{ flex: "1 1 auto" }}>
             Action Matches
           </div>
         </div>
@@ -34,7 +54,7 @@ const StmRulesTabRules: FunctionComponent = () => {
       <div className={styles.panelBottom}>
         <STMRulesTable />
       </div>
-    </>
+    </div>
   );
 };
 
