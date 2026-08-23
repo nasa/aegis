ab5eb31f4871fbead1b87ed58f7f375d330df86c
Ben Feist <benjamin.f.feist@nasa.gov>
2026-08-23 12:31:56 -0400

    Change slope to sequential color ramp

--- src/components/interface/measure/measure-hover.tsx
+++ src/components/interface/measure/measure-hover.tsx
@@ -10,7 +10,7 @@ const MeasureHoverValues: FunctionComponent<{
     <div className={measureStyles.measureHoverContainer}>
       <div className={timelineStyles.timelineHoverValues}>
         <div className={timelineStyles.timelineHoverValueItem}>
-          <div className={timelineStyles.timelineHoverValueTitle}>Total Distance (m)</div>
+          <div className={timelineStyles.timelineHoverValueTitle}>Tot Distance (m)</div>
           <div className={timelineStyles.timelineHoverValue}>
             {hoverValues.totalDistanceMeters?.toFixed(0)}
           </div>
@@ -22,7 +22,7 @@ const MeasureHoverValues: FunctionComponent<{
           </div>
         </div>
         <div className={timelineStyles.timelineHoverValueItem}>
-          <div className={timelineStyles.timelineHoverValueTitle}>Relative Elevation (m)</div>
+          <div className={timelineStyles.timelineHoverValueTitle}>Rel Elev (m)</div>
           <div className={timelineStyles.timelineHoverValue}>
             {hoverValues.elevationMeters?.toFixed(0)}
           </div>
