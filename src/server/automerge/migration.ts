14461015cff08175ca45fcf14e170bb7bd1398ad
Ben Feist <bf@benfeist.com>
2026-09-02 10:25:40 -0400

    Add automerge migration for required absolute slope array

--- src/server/automerge/migration.ts
+++ src/server/automerge/migration.ts
@@ -781,6 +781,19 @@ getORM()
       });
     };
 
+    // Migration: initialize absolute terrain slopes on traverses created before the field existed.
+    const automergeMigration20260902AddTraverseAbsoluteSlopes = async (
+      docHandle: DocHandle<Mission>
+    ) => {
+      docHandle.change((mission: Mission) => {
+        for (const traverse of Object.values(mission.traverses ?? {})) {
+          if (!Object.prototype.hasOwnProperty.call(traverse, "pathSegmentAbsoluteSlopes")) {
+            traverse.pathSegmentAbsoluteSlopes = null;
+          }
+        }
+      });
+    };
+
     serverLogger.debug({ logId: "automerge-migration", logValue: "Starting migrations..." });
     // Add migration functions to the list and run all the migrations on every doc
     const migrationFunctions: ((docHandle: DocHandle<Mission>) => Promise<void>)[] = [
@@ -791,6 +804,7 @@ getORM()
       automergeMigration20260809AddGridRenderMode,
       automergeMigration20260810RenameStationLabelStrokeToHalo,
       automergeMigration20260806XgressStations,
+      automergeMigration20260902AddTraverseAbsoluteSlopes,
     ];
     // Run all the migrations in the list above
     for (const func of migrationFunctions) {
