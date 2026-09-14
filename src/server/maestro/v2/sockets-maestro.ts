5206dc203e9d84e775e52497626f839fba5769b2
Jackie Vu <jackie.vu@nasa.gov>
2026-09-14 16:53:57 -0500

    rename import

--- src/server/maestro/v2/sockets-maestro.ts
+++ src/server/maestro/v2/sockets-maestro.ts
@@ -18,7 +18,7 @@ import {
 } from "server/maestro/v2/sockets-maestro-emitters";
 import { opUpdateMdau } from "server/maestro/v2/operations/op-mdau";
 import { mdauDataValidator } from "server/maestro/v2/mdauValidator";
-import { mdauValidator as mdauSchemaValidator } from "utils/validateSchemaServer";
+import { mdauSchemaValidator } from "utils/validateSchemaServer";
 import { emssTokenIsValid } from "utils/permissions";
 import { buildAegisSliceForMaestro } from "server/maestro/v2/buildAegisSlice";
 import { getAutomergeMissions } from "server/express/routes/missionAutomerge";
